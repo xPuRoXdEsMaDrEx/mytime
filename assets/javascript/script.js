31bc437a4e49d0b5501b74115da54f62966c01a8
Ivan Rodriguez <sickpunkrocker@gmail.com>
2022-09-18 21:02:18 -0700

    Added time block to html

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+
+
+for (var i = 0; i < currentQ.choices.length; i++) {
+    var choiceBtn = document.createElement('button');
+    choiceBtn.setAttribute('value');
+
+}
+// will log the button that contains the choices in the array assigned
+userChoices.addEventListener('click', function(event) {
+    console.log(event.target);
+})
